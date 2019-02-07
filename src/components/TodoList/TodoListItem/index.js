db1ac6b5ef91b2b01d6d71725b03a87c51032743
levan gulisashvili <levan.gulisashvili@gmail.com>
2019-02-07 05:11:59 +0400

    added styling to get done and active tasks

--- src/components/TodoList/TodoListItem/index.js
+++ src/components/TodoList/TodoListItem/index.js
@@ -6,10 +6,25 @@ export default class TodoListItem extends Component {
   render() {
     const {done, text} = this.props
 
+    const checkBoxStyle = {
+      background: done ? '#ba83ca': '#f1f1f1'
+    }
+
+    const listItemTextStyle = {
+      textDecoration: done ? 'line-through': null
+    }
+
     return (
       <div className="todo-list-item">
-        <span className="todo-list-item-check">{done}</span>
-        <span className="todo-list-item-text">{text}</span>
+        <span className="todo-list-item-check" style={checkBoxStyle}>
+          {done}
+        </span>
+        <span 
+          className="todo-list-item-text"
+          style={listItemTextStyle}
+        >
+          {text}
+        </span>
       </div>
     )
   }
